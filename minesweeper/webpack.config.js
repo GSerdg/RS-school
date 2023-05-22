ec9b385f3863f9720c8352da6266e115769b30e9
GSerdg <grebelsergey@mail.ru>
2023-05-23 00:02:51 +0300

    fix: change webpack configs

--- minesweeper/webpack.config.js
+++ minesweeper/webpack.config.js
@@ -5,6 +5,7 @@ const CopyPlugin = require('copy-webpack-plugin');
 module.exports = [
   {
     entry: './src/index.js',
+    node: false,
     output: {
       path: path.resolve(__dirname, 'dist'),
       filename: 'main.js',
