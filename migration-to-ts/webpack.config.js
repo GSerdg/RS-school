70dab2f4f1cc22b86b9cb802711eb7ba4b0c3877
GSerdg <grebelsergey@mail.ru>
2023-06-06 16:22:45 +0300

    fix: change config files to fix problem  working ESlinter

--- migration-to-ts/webpack.config.js
+++ migration-to-ts/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const EslingPlugin = require('eslint-webpack-plugin');
 
 const baseConfig = {
     entry: path.resolve(__dirname, './src/index'),
@@ -26,6 +27,7 @@ const baseConfig = {
         path: path.resolve(__dirname, '../dist'),
     },
     plugins: [
+        new EslingPlugin({ extensions: 'ts' }),
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, './src/index.html'),
             filename: 'index.html',
