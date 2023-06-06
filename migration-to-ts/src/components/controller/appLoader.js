28546bb41c620dc7228316c9b3eb708217edf6de
GSerdg <grebelsergey@mail.ru>
2023-06-06 12:52:34 +0300

    feat: add apiKey from AppLoader

--- migration-to-ts/src/components/controller/appLoader.js
+++ migration-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'b5b55592024f49b0a4c0bd22ee4637e9', // получите свой ключ https://newsapi.org/
         });
     }
 }
