7c317934e5795e5de0b71cb3ab016052834eeb74
GSerdg <grebelsergey@mail.ru>
2023-06-08 13:57:18 +0300

    refactor: delete script connection in index.html

--- migration-to-ts/webpack.dev.config.js
+++ migration-to-ts/webpack.dev.config.js
@@ -1,16 +1,16 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    /* devServer: {
-        contentBase: path.resolve(__dirname, '../dist'),
-    }, */
-    devServer: {
-        static: {
-            directory: path.join(__dirname, '../dist'),
-        },
-        compress: true,
-        port: 9000,
+  mode: 'development',
+  devtool: 'inline-source-map',
+  /* devServer: {
+      contentBase: path.resolve(__dirname, '../dist'),
+  }, */
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '../dist'),
     },
+    compress: true,
+    port: 9000,
+  },
 };
