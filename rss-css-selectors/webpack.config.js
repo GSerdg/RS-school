2a864e34f623e97f1e7a1ed127f0bd4ffcd27c4e
GSerdg <grebelsergey@mail.ru>
2023-06-16 15:13:26 +0300

    init: start task

---
+++ rss-css-selectors/webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const EslingPlugin = require('eslint-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+const baseConfig = {
+  entry: path.resolve(__dirname, './src/index'),
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/i,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+          {
+            loader: 'sass-resources-loader',
+            options: {
+              resources: [
+                'src/styles/abstract/_variables.scss',
+                'src/styles/abstract/_constants.scss',
+                'src/styles/abstract/_mixins.scss',
+                'src/styles/abstract/_placeholders.scss',
+              ],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  plugins: [
+    new EslingPlugin({ extensions: 'ts' }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'index.html',
+    }),
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, './src/icons'),
+          to: path.resolve(__dirname, './dist/icons'),
+        },
+      ],
+    }),
+  ],
+};
+
+module.exports = ({ mode }) => {
+  const isProductionMode = mode === 'prod';
+  const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+
+  return merge(baseConfig, envConfig);
+};
