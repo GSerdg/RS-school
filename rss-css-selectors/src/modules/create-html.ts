cd99b8f5cfda7238ee6bab011087be8e6cc4e74f
GSerdg <grebelsergey@mail.ru>
2023-06-19 13:33:21 +0300

    feat: add selection light on HTML text when mouse hover

--- rss-css-selectors/src/modules/create-html.ts
+++ rss-css-selectors/src/modules/create-html.ts
@@ -1,7 +1,7 @@
 import { createElement } from './createElement';
 import { Tag } from '../types/types';
 
-function createLevelHtml(data: Tag[], padding: number) {
+function createLevelHtml(data: Tag[]) {
   const ELEMENTS: HTMLElement[] = [];
 
   data.forEach((item) => {
@@ -17,33 +17,47 @@ function createLevelHtml(data: Tag[], padding: number) {
     }
     if (item.inside) {
       TEXT_OPEN += '>';
-      CHILD_ELEMENTS = createLevelHtml(item.inside, padding + 30);
+      CHILD_ELEMENTS = createLevelHtml(item.inside);
     } else {
       TEXT_OPEN += ' />';
     }
 
-    const STR_OPEN = createElement('p', ['code__tag'], undefined, TEXT_OPEN);
-    STR_OPEN.setAttribute('style', `padding-left: ${padding}px`);
+    const STR_OPEN = createElement('p', ['tag', 'tag_open']);
+    const STR_OPEN_SPAN = createElement('span', ['tag__text', 'tag__text_open'], undefined, TEXT_OPEN);
+    STR_OPEN.append(STR_OPEN_SPAN);
+    STR_OPEN.setAttribute('style', `padding-left: 30px`);
     ELEMENTS.push(STR_OPEN);
     if (CHILD_ELEMENTS) {
-      ELEMENTS.push(...CHILD_ELEMENTS);
-      const STR_CLOSED = createElement('p', ['code__tag'], undefined, TEXT_CLOSED);
-      STR_CLOSED.setAttribute('style', `padding-left: ${padding}px`);
-      ELEMENTS.push(STR_CLOSED);
+      const STR_CLOSED = createElement('p', ['tag', 'tag_closed']);
+      const STR_CLOSED_SPAN = createElement('span', ['tag__text', 'tag__text_closed'], undefined, TEXT_CLOSED);
+
+      STR_OPEN.append(...CHILD_ELEMENTS);
+      STR_CLOSED.append(STR_CLOSED_SPAN);
+      STR_OPEN.append(STR_CLOSED);
     }
   });
 
   return ELEMENTS;
 }
 
+function addSelectForElements(event: Event) {
+  const target = event.target as HTMLElement;
+  if (target.tagName !== 'SPAN') return;
+
+  const parentElement = target.closest('.tag_open');
+  parentElement?.classList.add('tag_light');
+}
+
 export function createViewHtml(data: Tag[]) {
   const TEXT_FOARM_CODE = createElement('div', ['text-foarm__code']);
-  const STR_1_OPEN = createElement('p', ['code__tag'], 'open-1', '<div class="table">');
-  const STR_1_CLOSED = createElement('p', ['code__tag'], 'closed-1', '</div>');
+  const STR_1_OPEN = createElement('p', ['tag', 'tag_open'], 'open-1', '<div class="table">');
+  const STR_1_CLOSED = createElement('p', ['tag', 'tag_closed'], 'closed-1', '</div>');
 
   TEXT_FOARM_CODE.append(STR_1_OPEN);
-  TEXT_FOARM_CODE.append(...createLevelHtml(data, 30));
-  TEXT_FOARM_CODE.append(STR_1_CLOSED);
+  STR_1_OPEN.append(...createLevelHtml(data));
+  STR_1_OPEN.append(STR_1_CLOSED);
 
+  TEXT_FOARM_CODE.addEventListener('mouseover', addSelectForElements);
+  // TEXT_FOARM_CODE.addEventListener('mouseout', removeSelectForElements);
   return TEXT_FOARM_CODE;
 }
