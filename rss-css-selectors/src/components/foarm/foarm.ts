eb1a19b2f529db952ecdcd8feab46eff7ca6da8a
GSerdg <grebelsergey@mail.ru>
2023-06-20 18:02:03 +0300

    add: shutdown strobe animation then write text in input

--- rss-css-selectors/src/components/foarm/foarm.ts
+++ rss-css-selectors/src/components/foarm/foarm.ts
@@ -4,4 +4,17 @@ import { findDomElement } from '../../modules/find-dom-element';
 import { levelData } from '../../modules/level-data';
 const level = 2;
 const VIEWER = findDomElement(document.body, '.text-foarm_viewer');
+const INPUT = findDomElement(document.body, '.input');
+
+function addRemoveInputStrobe(event: Event) {
+  const target = event.target as HTMLInputElement;
+  if (target.tagName !== 'INPUT') return;
+  if (target.value.length > 0) {
+    target.classList.remove('input_strobe');
+  } else {
+    target.classList.add('input_strobe');
+  }
+}
+
 VIEWER.append(createViewHtml(levelData[level]));
+INPUT.addEventListener('input', addRemoveInputStrobe);
